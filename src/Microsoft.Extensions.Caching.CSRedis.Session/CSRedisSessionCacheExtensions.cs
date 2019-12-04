81f85ef8baf8230474a3ee5460373e61ec9bedfc
其锋 叶 <yeqifeng2288@live.cn>
2019-12-04 15:06:18 +0800

    删除无用的方法。

--- src/Microsoft.Extensions.Caching.CSRedis.Session/CSRedisSessionCacheExtensions.cs
+++ src/Microsoft.Extensions.Caching.CSRedis.Session/CSRedisSessionCacheExtensions.cs
@@ -10,40 +10,6 @@ namespace Microsoft.Extensions.Caching.CSRedis.Session
     public static class CSRedisSessionCacheExtensions
     {
         /// <summary>
-        /// 添加特定的RedisSession。
-        /// </summary>
-        /// <returns></returns>
-        public static IServiceCollection AddRedisSession(this IServiceCollection services)
-        {
-            services.TryAddTransient<ISessionStore, DistributedRedisSessionStore>();
-            services.AddDataProtection();
-            return services;
-        }
-
-        /// <summary>
-        /// 添加RedisSession。
-        /// </summary>
-        /// <param name="services">服务。</param>
-        /// <param name="configure">Session配置。</param>
-        /// <returns></returns>
-        public static IServiceCollection AddRedisSession(this IServiceCollection services, Action<SessionOptions> configure)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            if (configure == null)
-            {
-                throw new ArgumentNullException(nameof(configure));
-            }
-
-            services.Configure(configure);
-            services.AddRedisSession();
-            return services;
-        }
-
-        /// <summary>
         /// 添加RedisSession。
         /// </summary>
         /// <param name="services">服务。</param>
@@ -68,8 +34,8 @@ namespace Microsoft.Extensions.Caching.CSRedis.Session
             }
 
             services.AddSingleton<IDistributedSessionCache>(new CSRedisSessionCache(redisClient));
-
-            services.AddRedisSession();
+            services.TryAddTransient<ISessionStore, DistributedRedisSessionStore>();
+            services.AddDataProtection();
             return services;
         }
     }
