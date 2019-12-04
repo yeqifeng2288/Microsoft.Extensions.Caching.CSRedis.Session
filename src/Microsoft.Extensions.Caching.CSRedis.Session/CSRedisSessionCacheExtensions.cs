b7eda627e9999a1f55e82a0f4740dd2b9563c9c4
其锋 叶 <yeqifeng2288@live.cn>
2019-12-04 14:48:00 +0800

    不需要额外注入接口实现。

--- src/Microsoft.Extensions.Caching.CSRedis.Session/CSRedisSessionCacheExtensions.cs
+++ src/Microsoft.Extensions.Caching.CSRedis.Session/CSRedisSessionCacheExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CSRedis;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Session;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -38,6 +39,36 @@ namespace Microsoft.Extensions.Caching.CSRedis.Session
             }
 
             services.Configure(configure);
+            services.AddRedisSession();
+            return services;
+        }
+
+        /// <summary>
+        /// 添加RedisSession。
+        /// </summary>
+        /// <param name="services">服务。</param>
+        /// <param name="redisClient">redis客户端。</param>
+        /// <param name="configure">Session配置。</param>
+        /// <returns></returns>
+        public static IServiceCollection AddRedisSession(this IServiceCollection services, CSRedisClient redisClient, Action<SessionOptions> configure = null)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (redisClient == null)
+            {
+                throw new ArgumentNullException(nameof(redisClient));
+            }
+
+            if (configure != null)
+            {
+                services.Configure(configure);
+            }
+
+            services.AddSingleton<IDistributedSessionCache>(new CSRedisSessionCache(redisClient));
+
             services.AddRedisSession();
             return services;
         }
